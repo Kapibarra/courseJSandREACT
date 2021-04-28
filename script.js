bbd62dced189b0e40f1dce6a65308e28986d2b3c
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-04-28 13:48:40 +0300

    init new lessons

---
+++ script.js
@@ -0,0 +1,20 @@
+const numberOfFilms = +prompt("How much films?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: true,
+};
+
+
+const a = prompt("which  films?", ""),
+b = prompt("Raiting?", ""),
+c = prompt("which films?", ""),
+d = prompt("Raiting?", "");
+
+personalMovieDB.movies[a] = b
+personalMovieDB.movies[c] = d
+
+console.log(personalMovieDB);
